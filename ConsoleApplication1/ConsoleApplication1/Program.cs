99c8dd8722d08762286a12000f60bf208063448a
javorek <kjavorek@email.com>
2017-03-24 09:59:15 +0100

    promjena u aplikaciji

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Pozdrav iz aplikacije od Kristine Javorek");
+            Console.WriteLine("Pozdrav iz aplikacije od Kristine Javorek, R3577");
         }
     }
 }
